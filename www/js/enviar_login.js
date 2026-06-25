f84578d4a9b6f2a5d6b43d5677b67f9f9d77c482
Juvam Rodrigues <juvamrodrigues17@gmail.com>
2026-06-24 22:53:47 -0300

    Criação de função de logar com a API rodando localmente.

---
+++ www/js/enviar_login.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const formLogin = document.getElementById("formLogin");
+
+    formLogin.addEventListener("submit", async function (event) {
+        //Espera antes de recarregar a página
+        event.preventDefault(); 
+
+        //Pegando email e senha do form e colocando numa variavel dados
+        var email = document.getElementById("email").value;
+        var senha = document.getElementById("senha").value
+        var dados = {
+            email: email,
+            senha: senha
+        };
+
+        //tentando se conectar com a api e mandar o objeto "dados"
+        try {
+            const resposta = await fetch("http://localhost:8080/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(dados)
+            });
+
+            if (!resposta.ok) {
+                throw new Error("Erro ao fazer login");
+            }
+
+            const idUsuario = await resposta.text();
+            console.log("ID do usuário:", idUsuario);
+
+            alert("Usuário logado com sucesso!");
+
+        } catch (erro) {
+            console.error(erro);
+            alert("Erro.");
+        }
+    });
+});
